f81bb7dd13dfcd42cd32ea2a9d962cc2437bda3b
Faiaz Khan <118570920+faiazkhan09@users.noreply.github.com>
2024-12-19 05:09:51 +0600

    Update form.py

--- flaskpro/form.py
+++ flaskpro/form.py
@@ -8,20 +8,19 @@ from flaskpro.models import User
 
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()]) #to verify email perform-> pip install email_validator. no need to import it. works just by ONLY installing <-
+    email = StringField('Email', validators=[DataRequired(), Email()]) 
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
 
-    # def __init__(self, *args, **kwargs):
-    #     super(SignupForm, self).__init__(*args, **kwargs)
+    
 
-    def validate_username(self, username):       #the name of this function has to be validate_'field'. anything else would throw an error.
-        user = User.query.filter_by(username=username.data).first() #the first username is the current info entered by the user & the 2nd username.data is the username stored in database
+    def validate_username(self, username):       
+        user = User.query.filter_by(username=username.data).first() 
         if user:
             raise ValidationError('This username already exists! Use a different username')
         
-    def validate_email(self, email):             #the name of this function has to be validate_'field'. anything else would throw an error.
+    def validate_email(self, email):             
         user = User.query.filter_by(email=email.data).first() 
         if user:
             raise ValidationError('Use a different Email!')
@@ -34,20 +33,19 @@ class LoginForm(FlaskForm):
 
 class AccountUpdateForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()]) #to verify email perform-> pip install email_validator. no need to import it. works just by ONLY installing <-
+    email = StringField('Email', validators=[DataRequired(), Email()]) 
     picture = FileField('Update Profile Pricture',validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
-    # def __init__(self, *args, **kwargs):
-    #     super(SignupForm, self).__init__(*args, **kwargs)
 
-    def validate_username(self, username):       #the name of this function has to be validate_'field'. anything else would throw an error.
+
+    def validate_username(self, username):       
         if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first() #the first username is the current info entered by the user & the 2nd username.data is the username stored in database
+            user = User.query.filter_by(username=username.data).first() 
             if user:
                 raise ValidationError('This username already exists! Use a different username')
         
-    def validate_email(self, email):             #the name of this function has to be validate_'field'. anything else would throw an error.
+    def validate_email(self, email):             
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first() 
             if user:
